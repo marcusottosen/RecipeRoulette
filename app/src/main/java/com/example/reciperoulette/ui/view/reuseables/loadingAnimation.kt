6fe2d5aab8400d9a3fc443c57625b390a2c80562
marcus <mkottosen@gmail.com>
2023-09-29 14:03:01 +0200

    Loading animation updated

--- app/src/main/java/com/example/reciperoulette/ui/view/reuseables/loadingAnimation.kt
+++ app/src/main/java/com/example/reciperoulette/ui/view/reuseables/loadingAnimation.kt
@@ -1,5 +1,8 @@
-import android.util.Log
-import androidx.compose.foundation.background
+import android.media.MediaPlayer
+import android.media.browse.MediaBrowser
+import android.net.Uri
+import android.view.SurfaceHolder
+import android.view.SurfaceView
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,33 +11,39 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.reciperoulette.R
 import com.example.reciperoulette.data.util.NavigationRoute
 import kotlinx.coroutines.delay
 
+
+
 @Composable
 fun AnimatedView(navController: NavController) {
     val composition by rememberLottieComposition(
-        LottieCompositionSpec.RawRes(R.raw.loading)
+        LottieCompositionSpec.RawRes(R.raw.loading_plate)
     )
-    val speed = 2.5f
+
+    val speed = 2f
 
     // Text visibility states
     var isFindingRecipe by remember { mutableStateOf(true) }
@@ -55,7 +64,7 @@ fun AnimatedView(navController: NavController) {
                 composition,
                 modifier = Modifier.size(300.dp).padding(bottom = 20.dp),
                 speed = speed,
-                iterations = 999
+                iterations = 3
             )
             if (isFindingRecipe) {
                 Text(
@@ -74,18 +83,18 @@ fun AnimatedView(navController: NavController) {
 
     // Use LaunchedEffect with delays to control the text animation
     LaunchedEffect(key1 = composition) {
-        delay(500) //500 for all
-        updateString = "Finding the best random recipe for you."
+        delay(1500) //500 for all
+        //updateString = "Finding the best random recipe for you"
 
-        delay(500)
-        updateString = "Finding the best random recipe for you.."
-
-        delay(500)
-        updateString = "Finding the best random recipe for you..."
-
-        delay(500)
+        //delay(300)
+        //updateString = "Finding the best random recipe for you.."
+//
+        //delay(300)
+        //updateString = "Finding the best random recipe for you..."
+//
+        //delay(300)
         isFindingRecipe = false
-        //navController.navigate(NavigationRoute.Recipe.route)
+        navController.navigate(NavigationRoute.Recipe.route)
        // {
        //     popUpTo(NavigationRoute.Homepage.route) {
        //         inclusive = false
